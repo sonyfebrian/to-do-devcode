718e061223f1a0f133624a2326b2efd9bb0f6120
s <s@localhost.localdomain>
2023-02-14 16:39:02 +0700

    add data

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,16 @@ import React from "react";
 export default function Navbar() {
   return (
     <div>
-      <nav className="flex items-center justify-between flex-wrap bg-[#16ABF8] p-6">
+      <nav
+        data-cy="header-background"
+        className="flex items-center justify-between flex-wrap bg-[#16ABF8] p-6"
+      >
         <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div className="text-sm lg:flex-grow">
-            <span className="font-semibold text-base text-white tracking-tight font-poppins m-48">
+            <span
+              data-cy="header-title"
+              className="font-semibold text-base text-white tracking-tight font-poppins m-48 pl-2 pt-2"
+            >
               To Do List App
             </span>
           </div>
