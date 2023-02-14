4dc0894775f4dd7261c5eaeec331e9cf0a83925a
s <s@localhost.localdomain>
2023-02-14 23:37:23 +0700

    fixed

--- src/components/ListToDo.js
+++ src/components/ListToDo.js
@@ -35,11 +35,7 @@ const TutorialsList = () => {
   let navigate = useNavigate();
 
   const deleteTutorial = (id) => {
-    TutorialDataService.deleteActivitygroup(id);
-    Swal.fire({
-      icon: "warning",
-      html: "Apakah anda yakin menghapus activity <br/> <b>metting dengan client</b>, ",
-    })
+    TutorialDataService.deleteActivitygroup(id)
 
       .then((response) => {
         console.log(response.data, "tes delete");
@@ -97,13 +93,18 @@ const TutorialsList = () => {
       <div class="flex flex-wrap justify-center align-center">
         {tutorials &&
           tutorials.map((tutorial, index) => (
-            <div className="p-4 md:w-1/4" key={index} data-cy="activity-item">
-              <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+            <div className="p-4 md:w-1/4" key={index}>
+              <div
+                className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden"
+                data-cy="activity-item"
+              >
                 <div className="w-full">
                   <div className="w-full flex p-2 mb-10">
-                    <div className="pl-2 pt-2 " data-cy="activity-title">
+                    <div className="pl-2 pt-2 ">
                       <Link to={`/detail/${tutorial.id}`}>
-                        <p className="font-bold">{tutorial.title}</p>
+                        <h4 data-cy="activity-title" className="font-bold">
+                          {tutorial.title}
+                        </h4>
                       </Link>
                     </div>
                   </div>
@@ -112,16 +113,21 @@ const TutorialsList = () => {
                 <div className="p-4">
                   <div className="flex items-center flex-wrap ">
                     <a href="/" className="text-green-800  md:mb-2 lg:mb-0">
-                      <p
-                        className="inline-flex items-center"
-                        data-cy="activity-item-date"
-                      >
-                        <Moment format="D MMMM YYYY">{date}</Moment>
+                      <p className="inline-flex items-center">
+                        <Moment
+                          data-cy="activity-item-date"
+                          format="D MMMM YYYY"
+                        >
+                          {date}
+                        </Moment>
                       </p>
                     </a>
                     <span className="text-gray-400 mr-3  items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 "></span>
-                    <span className="text-gray-400 inline-flex items-center leading-none text-sm">
-                      <button onClick={() => deleteTutorial(tutorial.id)}>
+                    <div className="text-gray-400 inline-flex items-center leading-none text-sm">
+                      <button
+                        onClick={() => deleteTutorial(tutorial.id)}
+                        data-cy="activity-item-delete-button"
+                      >
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           className="h-6 w-6"
@@ -133,12 +139,11 @@ const TutorialsList = () => {
                           <path
                             strokeLinecap="round"
                             strokeLinejoin="round"
-                            data-cy="activity-item-delete-button"
                             d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                           />
                         </svg>
                       </button>
-                    </span>
+                    </div>
                   </div>
                 </div>
               </div>
