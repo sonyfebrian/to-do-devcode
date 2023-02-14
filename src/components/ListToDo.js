8d1ef95e9f7482065b5973e1062472998aabc0ce
s <s@localhost.localdomain>
2023-02-15 01:16:32 +0700

    fixed

--- src/components/ListToDo.js
+++ src/components/ListToDo.js
@@ -6,6 +6,7 @@ import Swal from "sweetalert2";
 
 const TutorialsList = () => {
   const [tutorials, setTutorials] = useState([]);
+  const [loading, setLoading] = useState(false);
   const initialActivityState = {
     title: "worht",
     email: "as@io.com",
@@ -17,16 +18,18 @@ const TutorialsList = () => {
       title: todo.title,
       email: todo.email,
     };
+    setLoading(true);
     TutorialDataService.createActivityGroup(data)
       .then((response) => {
         setTodo({
           title: response.data.title,
           email: response.data.email,
         });
-
+        setLoading(false);
         console.log(response.data);
       })
       .catch((e) => {
+        setLoading(false);
         console.log(e, "error");
       });
   };
@@ -53,15 +56,17 @@ const TutorialsList = () => {
     retrieveTutorials();
   }, []);
 
-  const retrieveTutorials = () => {
-    TutorialDataService.getActivityGroup()
-      .then((response) => {
+  const retrieveTutorials = async () => {
+    try {
+      setLoading(true);
+      TutorialDataService.getActivityGroup().then((response) => {
         setTutorials(response.data.data);
         console.log(response.data);
-      })
-      .catch((e) => {
-        console.log(e);
       });
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
   };
 
   const refreshList = () => {
@@ -70,85 +75,74 @@ const TutorialsList = () => {
 
   return (
     <>
-      <div
-        className="grid grid-cols-6 gap-4 mt-20"
-        data-cy="activity-add-button"
-      >
-        <div className="col-start-2 col-end-3 text-3xl font-bold">
-          <h1 data-cy="activity-title" className="">
-            Activity
-          </h1>
-        </div>
-        <div className="col-end-7 col-span-2">
+      <div className="container-md px-40 py-10">
+        <div class="flex justify-between">
+          <h1 data-cy="activity-title">Activity</h1>
           <button
             data-cy="activity-add-button"
             type="button"
             className="flex items-center rounded-lg bg-indigo-500 px-4 py-2 text-white bg-[#16ABF8] "
             onClick={saveActivity}
           >
-            + Tambah
+            {loading ? <>Loading..</> : <>+ Tambah</>}
           </button>
-        </div>
-      </div>
-      <div class="flex flex-wrap justify-center align-center">
-        {tutorials &&
-          tutorials.map((tutorial, index) => (
-            <div className="p-4 md:w-1/4" key={index}>
-              <div
-                className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden"
-                data-cy="activity-item"
-              >
-                <div className="w-full">
-                  <div className="w-full flex p-2 mb-10">
-                    <div className="pl-2 pt-2 ">
-                      <Link to={`/detail/${tutorial.id}`}>
-                        <h4 data-cy="activity-title" className="font-bold">
-                          {tutorial.title}
-                        </h4>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="p-4">
-                  <div className="flex items-center flex-wrap ">
-                    <a href="/" className="text-green-800  md:mb-2 lg:mb-0">
-                      <p className="inline-flex items-center">
-                        <Moment
-                          data-cy="activity-item-date"
-                          format="D MMMM YYYY"
-                        >
-                          {date}
-                        </Moment>
-                      </p>
-                    </a>
-                    <span className="text-gray-400 mr-3  items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 "></span>
-                    <div className="text-gray-400 inline-flex items-center leading-none text-sm">
-                      <button
-                        onClick={() => deleteTutorial(tutorial.id)}
-                        data-cy="activity-item-delete-button"
+        </div>{" "}
+        <div className="flex flex-row m-2">
+          {loading ? (
+            <>Loading</>
+          ) : (
+            <>
+              <div className="flex flex-wrap -m-4">
+                {tutorials &&
+                  tutorials.map((tutorial, index) => (
+                    <div className="p-4 lg:w-1/3" key={index}>
+                      <div
+                        className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden"
+                        data-cy="activity-item"
                       >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-6 w-6"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          strokeWidth={2}
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                          />
-                        </svg>
-                      </button>
+                        <Link to={`/detail/${tutorial.id}`}>
+                          <h4
+                            data-cy="activity-title"
+                            className="font-bold m-4"
+                          >
+                            {tutorial.title}
+                          </h4>
+                        </Link>
+                        <div className="flex justify-between m-4">
+                          <Moment
+                            data-cy="activity-item-date"
+                            format="D MMMM YYYY"
+                          >
+                            {date}
+                          </Moment>
+
+                          <button
+                            onClick={() => deleteTutorial(tutorial.id)}
+                            data-cy="activity-item-delete-button"
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="h-6 w-6"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                              strokeWidth={2}
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                              />
+                            </svg>
+                          </button>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
+                  ))}
               </div>
-            </div>
-          ))}
+            </>
+          )}
+        </div>
       </div>
     </>
   );
